fc0277514bc29007fb5c35f7217c0ce2ba3d9f94
ivanbokii <bokiyis@gmail.com>
2015-05-04 22:29:35 +0100

    complete rewrite of handlers

--- project.clj
+++ project.clj
@@ -3,11 +3,11 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [sonian/carica "1.1.0"]
                  [im.chit/cronj "1.4.1"]
                  [org.clojure/data.json "0.2.6"]
                  [com.taoensso/timbre "3.4.0"]
-                 [com.climate/java.shell2 "0.1.0"]]
+                 [com.climate/java.shell2 "0.1.0"]
+                 [clj-time "0.9.0"]]
   :main ^:skip-aot lopare.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}
--- src/lopare/handlers.clj
+++ src/lopare/handlers.clj
@@ -1,42 +1,56 @@
 (ns lopare.handlers
   (:require [clojure.java.shell2 :as shell]
             [clojure.data.json :as json]
-            [taoensso.timbre :as timbre]))
+            [taoensso.timbre :as timbre]
+            [clj-time.local :as time]))
 
 (timbre/refer-timbre)
 
-(defn run-shell
-  [job-config additional-param]
+(defn run
+  [config step]
+  (let [executable (clojure.string/split (:exec config) #" ")
+        entry (:entry config)
+        path-to-job-dir (str "./jobs/" (:name config))
+        shell-params (concat executable [entry (json/write-str config) step :dir path-to-job-dir])]
+    (apply shell/sh shell-params)))
+
+(defn error-handler
+  [fn]
   (try
-    (let [executable (clojure.string/split (:exec job-config) #" ")
-          entry (:entry job-config)
-          path-to-job-dir (str "./jobs/" (:name job-config))
-          shell-params (concat executable [entry (json/write-str job-config) additional-param :dir path-to-job-dir])
-          result (apply shell/sh shell-params)]
-      (if (not= (:exit result) 0)
-        (do
-          (error (:name job-config) additional-param "Error: " result)
-          (assoc job-config :error (:err result)))
-        job-config))
-    (catch Exception e (do
-                         (error e)
-                         {:name (:name job-config) :error e}))))
+    (let [result (fn)]
+      (when-not (= (:exit result) 0)
+        {:error result}))
+    (catch Exception e {:error e})))
 
-(defn pre-job
-  [time job-config]
-  (info "Starting job: " (:name job-config))
-  (run-shell job-config "pre"))
+(defn save-run
+  [run-result]
+  (spit "last-run" run-result))
 
-(defn run-job
-  [time job-config]
-  (info "Running job: " (:name job-config))
-  (if-not (:error job-config)
-    (run-shell job-config "")
-    (error (:name job-config) ": RUN is skipped because of error on the previous step")))
+(defn pre
+  [time config]
+  (let [execute (partial run config :pre)
+        result (error-handler execute)
+        error (:error result)]
+    (if error
+      {:config config :pre {:error error} :error true :start-time time}
+      {:config config :pre {:start-time time :end-time (time/local-now)} :start-time time})))
 
-(defn post-job
-  [time job-config]
-  (if-not (or (:error (:result job-config)) (:error job-config))
-    (let [result (run-shell job-config "post")]
-      (when-not (:error result) (info "Finished job: " (:name job-config))))
-    (error (:name job-config) ": POST is skipped because of error on the previous step")))
+(defn handler
+  [time pre-result]
+  (when-not (:error pre-result)
+    (let [execute (partial run (:config pre-result) :handler)
+          result (error-handler execute)
+          error (:error result)]
+      (if error
+        {:error error}
+        {:start-time time :end-time (time/local-now)}))))
+
+(defn post
+  [time handler-result]
+  (when-not (or (:error handler-result) (:error (:result handler-result)))
+    (let [execute (partial run (:config handler-result) :post)
+          result (error-handler execute)
+          error (:error result)]
+      (if error
+        (save-run (assoc handler-result :post {:error error} :end-time (time/local-now)))
+        (save-run (assoc handler-result :post {:start-time time :end-time (time/local-now)}))))))
